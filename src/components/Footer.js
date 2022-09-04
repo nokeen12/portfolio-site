082ef7556c871f1f6be3700e330438742fa49aa5
Nokeen <cordrepayne@gmail.com>
2022-09-04 18:34:46 -0500

    finish footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,20 @@
 import './style/Footer.scss'
-
+import email from '../images/email.png'
+import github from '../images/github.png'
+import linkedin from '../images/linkedin.png'
 function Footer(){
     return(
         <footer>
             <h2>Cordre Payne</h2>
             <h3>Full Stack Web Developer</h3>
-            <div>
-                
+            <div>         
+                <img src={email} alt="email"/>
+                <a href="https://github.com/nokeen12">
+                    <img src={github} alt="github"/>
+                </a>
+                <a href="https://www.linkedin.com/in/cordre-payne">
+                    <img src={linkedin} alt="linkedin"/>
+                </a>
             </div>
         </footer>
     )
