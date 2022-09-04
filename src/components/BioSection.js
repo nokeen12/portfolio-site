20eac327db7b8ca3c8700ba0dc007b5b8f7b750e
Nokeen <cordrepayne@gmail.com>
2022-09-04 17:40:27 -0500

    Add bio section download and link buttons

--- src/components/BioSection.js
+++ src/components/BioSection.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
 import './style/BioSection.scss'
 
 function BioSection(){
     return(
         <div id="bio-section">
-            BioSection
+            <div id="bio-buttons">
+                <a href="../data/Resume.pdf" download>Download CV</a>
+                <Link to="/projects">My Work</Link>
+            </div>
         </div>
     )
 }
