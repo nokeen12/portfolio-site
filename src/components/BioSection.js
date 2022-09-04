8a79a50fd3c7b4f43589bb3ca197b72f58760d53
Nokeen <cordrepayne@gmail.com>
2022-09-04 17:51:48 -0500

    fix download link

--- src/components/BioSection.js
+++ src/components/BioSection.js
@@ -1,3 +1,4 @@
+import { colours } from 'nodemon/lib/config/defaults';
 import { Link } from 'react-router-dom';
 import './style/BioSection.scss'
 
@@ -5,8 +6,12 @@ function BioSection(){
     return(
         <div id="bio-section">
             <div id="bio-buttons">
-                <a href="../data/Resume.pdf" download>Download CV</a>
+                <a href="./Resume.pdf" download>Download CV</a>
                 <Link to="/projects">My Work</Link>
+            </div>
+            <div id="bio-details">
+                <div id="bio-img" style={{width: 400, height: 300, 'background-color': 'darkorange'}}/>
+                <div id="bio-text"><p>random rnadom random random random random random</p></div>
             </div>
         </div>
     )
