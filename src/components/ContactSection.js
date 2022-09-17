414817ac31c8c32008e57e405bb63fa56206d340
Nokeen <cordrepayne@gmail.com>
2022-09-17 18:43:59 -0500

    Made contact me section prettier

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -5,15 +5,9 @@ function ContactSection(){
         <div id="contact-section">
             <h2>Contact Me</h2>
             <form action="mailto:cordrepayne@gmail.com" method="post" enctype="text/plain">
-                Name/Company:<br/>
-                <input type="text" name="name" id="name" />
-                <br/>
-                Email:<br/>
-                <input type="text" name="email" id="email" />
-                <br/>
-                Message:<br/>
-                <input type="text" name="message" id="message"/>
-                <br/>
+                <input type="text" name="name" id="name" placeholder="Name" />
+                <input type="email" name="email" id="email" placeholder="Email" />
+                <textarea type="text" name="message" id="message" placeholder="Message" />
                 <input type="submit" value="Send" id='send'/>
             </form>
         </div>
