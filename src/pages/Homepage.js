1f8ca10498f017a079d0a04ed48295910387b94e
Nokeen <cordrepayne@gmail.com>
2022-11-03 01:40:03 -0500

    add repeat background

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,7 +3,7 @@ import HeroSection from '../components/HeroSection'
 import BioSection from '../components/BioSection'
 import ProjectsSection from '../components/ProjectsSection'
 import ContactSection from '../components/ContactSection'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 
 
 function HomePage(){
@@ -13,10 +13,22 @@ function HomePage(){
         document.addEventListener('mousemove', e =>{
             cursor.style = `top: ${e.pageY-50}px; left: ${e.pageX-50}px`
         })
+        for(let j=0; j < window.innerHeight/35; j++){
+            document.getElementById("bg-custom").innerHTML += `
+            <div id="n${j}" class="bg-row ${j%2? "offset": null}"></div>
+            `
+            for(let i=0; i < window.innerWidth/75+1; i++){
+                document.getElementById(`n${j}`).innerHTML += `
+                <div class="lattice"></div>
+                `
+            }
+        }       
     }, []);
     return(
         <div id="homepage">
             <div id="cursor"></div>
+            <div id="bg-custom">
+            </div>
             <HeroSection/>
             <BioSection/>
             <ProjectsSection/>
