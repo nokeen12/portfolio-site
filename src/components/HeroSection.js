6aaa20ea767c585f355fed94a2ef9579b69c3c94
Nokeen <cordrepayne@gmail.com>
2022-11-03 17:27:31 -0500

    flickering name and removed img

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,15 +1,11 @@
 import './style/HeroSection.scss'
-import Cordre from '../images/cordrep.png'
 function HeroSection(){
     return(
         <div id="hero-section">
             <div id="hero-text">
-                <h2>Cordre Payne</h2>
+                <h2>Co<element>r</element>dre Payne</h2>
                 <h3>Full Stack Web Developer</h3>
                 <p>Just a developer who loves a challenge.</p>
-            </div>
-            <div id="hero-img">
-                <img src={Cordre} alt="cordre"/>
             </div>
         </div>
     )
