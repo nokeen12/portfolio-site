f637b5209ca0cfe4657432aec6d383da1bafe5bf
Nokeen <cordrepayne@gmail.com>
2022-11-08 21:31:36 -0600

    Update bio

--- src/components/BioSection.js
+++ src/components/BioSection.js
@@ -11,13 +11,9 @@ function BioSection(){
             <div id="bio-details">
                 <p>
                     <img src='./me-and-abe.JPG' alt="cordre"/>
-                    The tech industry has always been interesting and a love of mine, and influenced my decision heavily in joining Ironhack. Now that I've graduated from the Ironhack bootcamp,
-                     I am looking to start working as a Front end, Back end, or Full Stack Web Developer.
-                    I have 2.5 years of experience in cooking, cleaning, and customer service meaning that I know how to follow directions, pay attention to detail, and the ability to communicate 
-                    effectively with customers. While working at Panda Express, I contributed to increasing our received donations by ~10% through having short conversations with the customers and 
-                    making them feel more welcomed. As I was put into a leadership role as a Cook/Shift Lead, I participated in training new side cooks and associates at 4 store locations. Shifting 
-                    my career from the labour and food industry to the tech industry was an amazing decision and my past experiences help me excel. Whether I'm bug-fixing, solving algorithms or 
-                    anything else, I'm always looking for ways to improve and better myself.
+                    I've always had a love for the tech industry and now I have the tools and skills to create it.
+                    Working for over 3 years in the labour and food industry intensified my love for Web Development and being able to take better care of my body.
+                    My past experiences give me a different outlook on projects and help me excel. Anything is achievable as I'm always looking for ways to improve and better myself.
                     <br/>
                     <br/>
                     Specialties: JavaScript, Node.js, React.js, MongoDB, Express, HTML, CSS, SCSS
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,7 +3,7 @@ function HeroSection(){
     return(
         <div id="hero-section">
             <div id="hero-text">
-                <h2>Co<element>r</element>dre Payne</h2>
+                <h2>Co<span>r</span>dre Payne</h2>
                 <h3>Full Stack Web Developer</h3>
                 <p>Just a developer who loves a challenge.</p>
             </div>
