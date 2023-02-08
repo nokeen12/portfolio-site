1a7ba90291680b3921f69961d01e166d615f593a
Nokeen <cordrepayne@gmail.com>
2023-02-07 23:25:47 -0600

    remove lattice background and cursor highlight

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -12,24 +12,25 @@ function HomePage(){
         document.addEventListener('mousemove', e =>{
             cursor.style = `top: ${e.pageY-50}px; left: ${e.pageX-50}px`
         })
-        for(let j=0; j < window.innerHeight/35; j++){ //creates lattice background pattern
-            document.getElementById("bg-custom").innerHTML += `
-            <div id="n${j}" class="bg-row ${j%2? "offset": null}"></div>
-            `
-            for(let i=0; i < window.innerWidth/75+1; i++){
-                document.getElementById(`n${j}`).innerHTML += `
-                <div class="lattice"></div>
-                `
-            }
-        }
+        // for(let j=0; j < window.innerHeight/35; j++){ //creates lattice background pattern
+        //     document.getElementById("bg-custom").innerHTML += `
+        //     <div id="n${j}" class="bg-row ${j%2? "offset": null}"></div>
+        //     `
+        //     for(let i=0; i < window.innerWidth/75+1; i++){
+        //         document.getElementById(`n${j}`).innerHTML += `
+        //         <div class="lattice"></div>
+        //         `
+        //     }
+        // }
     }, []);
     return(
         <div id="homepage">
-            <div id="cursor"></div>
+            {/* <div id="cursor"></div> */}
             <div id="bg-custom">
             </div>
             <HeroSection/>
             <BioSection/>
+            <span>Projects</span>
             <ProjectCarousel/>
             <ContactSection/>
         </div>
