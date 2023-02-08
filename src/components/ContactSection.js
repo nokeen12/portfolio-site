72c8c1d818a279d8116709b7e5d93ed1129650ac
Nokeen <cordrepayne@gmail.com>
2023-02-08 12:35:17 -0600

    Improve contact section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -2,9 +2,29 @@ import './style/ContactSection.scss'
 
 function ContactSection(){
     return(
+        // <div id="contact-section">
+        //     <h2>Contact Me</h2>
+        //     <a href="mailto:cordrepayne@gmail.com"><u>cordrepayne@gmail.com</u></a>
+        // </div>
         <div id="contact-section">
             <h2>Contact Me</h2>
-            <a href="mailto:cordrepayne@gmail.com"><u>cordrepayne@gmail.com</u></a>
+            <form id="contact-form">
+                <div className="inputBox">
+                    <input type="text" name="" required="required"></input>
+                    <span>Full Name</span>
+                </div>
+                <div className="inputBox">
+                    <input type="text" name="" required="required"></input>
+                    <span>Email</span>
+                </div>
+                <div className="inputBox">
+                    <textarea required="required"></textarea>
+                    <span>Type your Message...</span>
+                </div>
+                <div className="inputBox">
+                    <input type="submit" name="" value="Send"></input>
+                </div>
+            </form>
         </div>
     )
 }
