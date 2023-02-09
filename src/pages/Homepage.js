d839692ef3d95688d9b3991643661b7a6d5e41bd
Nokeen <cordrepayne@gmail.com>
2023-02-09 14:55:53 -0600

    Add spacing to sections

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -16,7 +16,7 @@ function HomePage(){
             <h1>My Work</h1>
             <ProjectSection/>
             <ContactSection/>
-            {/* <div id="next-section">V</div> */}
+            <div id="next-section">V</div>
         </div>
     )
 }
